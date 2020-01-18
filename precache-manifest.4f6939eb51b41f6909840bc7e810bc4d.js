2c0bcbf958579b7fd2e29fc6544e4ec14093036c
benlambert43 <benlambert43@gmail.com>
2020-01-18 16:20:58 -0500

    Updates

---
+++ precache-manifest.4f6939eb51b41f6909840bc7e810bc4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb301f8a4de0bcbae6eeb767d4b2250",
+    "url": "/RU-Plan-My-Major/index.html"
+  },
+  {
+    "revision": "63e8804ef4e953ad99fc",
+    "url": "/RU-Plan-My-Major/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "6baad9c2dc5d91fd230d",
+    "url": "/RU-Plan-My-Major/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "63e8804ef4e953ad99fc",
+    "url": "/RU-Plan-My-Major/static/js/2.94315352.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/RU-Plan-My-Major/static/js/2.94315352.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6baad9c2dc5d91fd230d",
+    "url": "/RU-Plan-My-Major/static/js/main.e98bfd69.chunk.js"
+  },
+  {
+    "revision": "2bf5a0950a5ed6d1a140",
+    "url": "/RU-Plan-My-Major/static/js/runtime-main.a8cdfb08.js"
+  }
+]);
